c602d1e3da09757fcb4cc19d8b7a17ecc59c7b01
arham <46757695+arhammxo@users.noreply.github.com>
2023-05-21 11:09:00 +0530

    Detecting faces using the DSFD approach
    Use the test.py and update the parameters accordingly to extract the faces from the required media,

---
+++ DSFD/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+import torch
+import torchvision
+
+torch_ver = [int(x) for x in torch.__version__.split(".")[:2]]
+assert torch_ver >= [1, 6], "Requires PyTorch >= 1.6"
+torchvision_ver = [int(x) for x in torchvision.__version__.split(".")[:2]]
+assert torchvision_ver >= [0, 3], "Requires torchvision >= 0.3"
+
+setuptools.setup(
+    name="face_detection",
+    version="0.2.1",
+    url="https://github.com/hukkelas/DSFD-Pytorch-Inference",
+    python_requires='>=3.6',
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "numpy",
+    ],
+    packages=setuptools.find_packages()
+)
